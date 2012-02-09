e461e098a6eead005454e5d0b4f43b4e1cdd4a65
Troy Stribling <troy.stribling@gmail.com>
2012-02-08 22:51:13 -0500

    I can only say that ffmpeg sucks

--- examples/rgb_file.c
+++ examples/rgb_file.c
@@ -1,5 +1,6 @@
 #include "libavcodec/avcodec.h"
 #include "libavformat/avformat.h"
+#include "libswscale/swscale.h"
 
 #include <stdio.h>
 
@@ -9,19 +10,11 @@
 #define FRAMES_PER_SECOND  30
 
 int main(int argc, char *argv[]) {
-  AVFormatContext *pFormatCtx;
-  int             i, videoStream;
-  AVCodecContext* pCodecCtx;
-  AVCodec*        pCodec;
-  AVFrame*        pFrame;
-  AVFrame*        pFrameRGB;
-  AVPacket        packet;
-  int             frameFinished;
-  int             numBytes;
-  char*           buffer;
-  int             frames;
-  int             fileLen;
-  FILE*           file;
+  char*               rgbBuffer;
+  int                 frames;
+  int                 rgbFileLen;
+  FILE*               rgbFile;
+  struct SwsContext*  pRgbToYuvContext;
 
   // check args
   if(argc < 2) {
@@ -32,111 +25,30 @@ int main(int argc, char *argv[]) {
 	// read file
   printf("ENCODING RGB FILE: %s\n", argv[1]);
   /*buffer = (char*)malloc(IMAGE_WIDTH*IMAGE_HEIGHT);*/
-  file = fopen(argv[1], "rb");
-  if (file == NULL) {
+  rgbFile = fopen(argv[1], "rb");
+  if (rgbFile == NULL) {
     printf("ERROR OPENING FILE\n");
     exit(-1);
   }
-  fseek(file, 0, SEEK_END);
-  fileLen=ftell(file);
-  fseek(file, 0, SEEK_SET);
-  frames = fileLen / (BYTES_PER_PIXEL * IMAGE_WIDTH * IMAGE_HEIGHT);
-  printf("FILE LENGTH: %d BYTES\n", fileLen);
+  fseek(rgbFile, 0, SEEK_END);
+  rgbFileLen = ftell(rgbFile);
+  fseek(rgbFile, 0, SEEK_SET);
+  frames = rgbFileLen / (BYTES_PER_PIXEL * IMAGE_WIDTH * IMAGE_HEIGHT);
+  printf("RGB FILE LENGTH: %d BYTES\n", rgbFileLen);
   printf("FRAMES: %d\n", frames);
 
   // Register all formats and codecs
   av_register_all();
 
-  // Open video file
-  /*if(av_open_input_file(&pFormatCtx, argv[1], NULL, 0, NULL) != 0)*/
-    /*return -1; // Couldn't open file*/
+  // create RGB to YUV conversion context
+  pRgbToYuvContext = sws_getContext(IMAGE_WIDTH, IMAGE_HEIGHT, PIX_FMT_RGB24, IMAGE_WIDTH, IMAGE_HEIGHT, PIX_FMT_YUV420P, SWS_BICUBIC, NULL, NULL, NULL);
 
-  // Retrieve stream information
-  /*if(av_find_stream_info(pFormatCtx) < 0)*/
-    /*return -1; // Couldn't find stream information*/
+  // convet frames
+  for (int i =0; i < frames; i++) {
+    sws_scale();
+  }
 
-  // Dump information about file onto standard error
-  /*dump_format(pFormatCtx, 0, argv[1], 0);*/
+  fclose(rgbFile);
 
-  // Find the first video stream
-  /*videoStream=-1;*/
-  /*for(i=0; i<pFormatCtx->nb_streams; i++)*/
-    /*if(pFormatCtx->streams[i]->codec->codec_type==CODEC_TYPE_VIDEO) {*/
-      /*videoStream=i;*/
-      /*break;*/
-    /*}*/
-  /*if(videoStream==-1)*/
-    /*return -1; // Didn't find a video stream*/
-
-  // Get a pointer to the codec context for the video stream
-  /*pCodecCtx=pFormatCtx->streams[videoStream]->codec;*/
-
-  // Find the decoder for the video stream
-  /*pCodec=avcodec_find_decoder(pCodecCtx->codec_id);*/
-  /*if(pCodec==NULL) {*/
-    /*fprintf(stderr, "Unsupported codec!\n");*/
-    /*return -1; // Codec not found*/
-  /*}*/
-  // Open codec
-  /*if(avcodec_open(pCodecCtx, pCodec)<0)*/
-    /*return -1; // Could not open codec*/
-
-  // Allocate video frame
-  /*pFrame=avcodec_alloc_frame();*/
-
-  // Allocate an AVFrame structure
-  /*pFrameRGB=avcodec_alloc_frame();*/
-  /*if(pFrameRGB==NULL)*/
-    /*return -1;*/
-
-  // Determine required buffer size and allocate buffer
-  /*numBytes=avpicture_get_size(PIX_FMT_RGB24, pCodecCtx->width,*/
-						/*pCodecCtx->height);*/
-  /*buffer=(uint8_t *)av_malloc(numBytes*sizeof(uint8_t));*/
-
-  // Assign appropriate parts of buffer to image planes in pFrameRGB
-  // Note that pFrameRGB is an AVFrame, but AVFrame is a superset
-  // of AVPicture
-  /*avpicture_fill((AVPicture *)pFrameRGB, buffer, PIX_FMT_RGB24,*/
-		 /*pCodecCtx->width, pCodecCtx->height);*/
-
-  // Read frames and save first five frames to disk
-  /*i=0;*/
-  /*while(av_read_frame(pFormatCtx, &packet)>=0) {*/
-    // Is this a packet from the video stream?
-    /*if(packet.stream_index==videoStream) {*/
-      // Decode video frame
-      /*avcodec_decode_video(pCodecCtx, pFrame, &frameFinished,*/
-				 /*packet.data, packet.size);*/
-
-      // Did we get a video frame?
-      /*if(frameFinished) {*/
-	// Convert the image from its native format to RGB
-	/*img_convert((AVPicture *)pFrameRGB, PIX_FMT_RGB24,*/
-                    /*(AVPicture*)pFrame, pCodecCtx->pix_fmt, pCodecCtx->width,*/
-                    /*pCodecCtx->height);*/
-
-	// Save the frame to disk
-	/*if(++i<=5)*/
-		/*SaveFrame(pFrameRGB, pCodecCtx->width, pCodecCtx->height,*/
-				/*i);*/
-      /*}*/
-    /*}*/
-
-    // Free the packet that was allocated by av_read_frame
-    /*av_free_packet(&packet);*/
-  /*}*/
-
-  // Free the RGB image
-  /*av_free(buffer);*/
-  /*av_free(pFrameRGB);*/
-
-  // Free the YUV frame
-  /*av_free(pFrame);*/
-
-  // Close the codec
-  /*avcodec_close(pCodecCtx);*/
-
-  fclose(file);
   return 0;
 }
